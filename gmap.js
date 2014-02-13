845a5e2592ba86c4bb7df85f20de7a5f440befa2
zivko <zivko.sudarski@gmail.com>
2014-02-14 11:38:14 +1300

    JQuery Plugin Code

--- gmap.js
+++ gmap.js
@@ -79,14 +79,14 @@
 		// A really lightweight plugin wrapper around the constructor,
 		// preventing against multiple instantiations
 		$.fn[ pluginName ] = function ( options ) {
-				this.each(function() {
-		 				if ( !$.data( this, "plugin_" + pluginName ) ) {
-						$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
-						}
-				});
+			  this.each(function() {
+		 	     if ( !$.data( this, "plugin_" + pluginName ) ) {
+			     $.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+			     }
+			 });
 
-				// chain jQuery functions
-				return this;
+			// chain jQuery functions
+			return this;
 		};
 
 })( jQuery, window, document );
